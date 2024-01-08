9cecbd649878f771f011926c83085e0d730133a0
arseny-smirnov <arseny.smiirnov@gmail.com>
2024-01-08 19:40:20 +0500

    add webpack config and fix translations

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,23 +3,29 @@ import cls from './Sidebar.module.scss';
 import React, {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 import {LangSwitcher} from "widgets/LangSwitcher";
+import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
     className?: string
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({className}) => {
-    const [collapsed, setCollapsed] = useState(false)
+    const {t} = useTranslation()
+
+    const [collapsed, setCollapsed] = useState(true)
+    const [buttonName, setButtonName] =
+        useState<'Развернуть' | 'Свернуть'>('Развернуть')
 
     const onToggle = () => {
         setCollapsed(prevState => !prevState)
+        buttonName === 'Развернуть' ? setButtonName('Свернуть') : setButtonName('Развернуть')
     }
 
     return (
         <div
             className={clsx(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-           <button onClick={onToggle}>toggle</button>
+           <button onClick={onToggle}>{t(buttonName)}</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher />
